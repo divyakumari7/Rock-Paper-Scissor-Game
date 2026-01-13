2886021b31120e171df0aaf3c0f9c197f00e7af2
divya kumari <divyakumari352932@gmail.com>
2026-01-13 20:18:39 +0530

    Completed

--- app/src/main/java/com/example/rockpaperscissorgame/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissorgame/RockPaperScissors.kt
@@ -4,7 +4,48 @@ fun main(){
     var computerChoice = ""
     var playerChoice = ""
 
-    print("Enter Rock, Paper or Scissor")
+    println("Enter Rock, Paper or Scissor")
     playerChoice = readln()
+
+    val randomNumber = (1 .. 3).random()
+
+
+
+//    if(randomNumber == 1){
+//        computerChoice = "Rock"
+//    }
+//    else if(randomNumber == 2){
+//        computerChoice = "Paper"
+//    }
+//    else if(randomNumber == 3){
+//        computerChoice = "Scissor"
+//    }
+
+
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissor"
+        }
+    }
+
+    println(computerChoice)
+
+    val winner = when{
+        playerChoice == computerChoice -> "Draw"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Rock" && computerChoice == "Scissor" -> "Player"
+        playerChoice == "Scissor" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+
+
+    if(winner == "Draw") print("Its a Draw")
+    else print("$winner Won!")
 
 }
