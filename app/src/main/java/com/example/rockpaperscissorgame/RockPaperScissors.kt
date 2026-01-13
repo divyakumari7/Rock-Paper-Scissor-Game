44bdafacef60f7c697e6a17f2f2c74095e6c0abc
divya kumari <divyakumari352932@gmail.com>
2026-01-13 19:25:19 +0530

    First Step

---
+++ app/src/main/java/com/example/rockpaperscissorgame/RockPaperScissors.kt
@@ -0,0 +1,10 @@
+package com.example.rockpaperscissorgame
+
+fun main(){
+    var computerChoice = ""
+    var playerChoice = ""
+
+    print("Enter Rock, Paper or Scissor")
+    playerChoice = readln()
+
+}
